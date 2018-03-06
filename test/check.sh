c7009d283c4316e90830006bed15cd90f3e53359
Ruben Koster <rkoster@starkandwayne.com>
2018-03-06 21:04:03 +0100

    Check should return an array

--- test/check.sh
+++ test/check.sh
@@ -6,14 +6,14 @@ source $(dirname $0)/helpers.sh
 
 it_can_check_empty_path() {
     _check "/empty_path" | \
-        jq -e '.version.md5 == "8a80554c91d9fca8acb82f023de02f11"'
+        jq -e '.[0].version.md5 == "8a80554c91d9fca8acb82f023de02f11"'
 }
 
 it_can_check_schema() {
     schema=$(jq -n -c '{properties: []}')
     _credhub set -n /check_schema/schema -t json -v "${schema}"
     _check "/check_schema" | \
-        jq -e '.version.md5 == "6917b7c95f545d7faf1d7c8d54a15e69"'
+        jq -e '.[0].version.md5 == "6917b7c95f545d7faf1d7c8d54a15e69"'
 }
 
 run it_can_check_empty_path
